46ae51afff7810a606b6dbd9c2576c8e3a346697
Laya-Sunil <78962385+Laya-Sunil@users.noreply.github.com>
2021-02-19 21:33:55 +0530

    js connection is done

--- WebApplicationMvc/Scripts/myScript.js
+++ WebApplicationMvc/Scripts/myScript.js
@@ -1,10 +1,15 @@
-﻿function validate() {
+﻿$(document).ready(function () {
+    alert("Hello");
+});
+
+function validate() {
     alert('java script');
     var mail = document.forms["register-form"]["EmailId"].value;
     var name = document.getElementById("uname").value;
     var nameRegex = /(^[A-Z][a-z]{3,9}$)/;
     if (!name.match(mailRegex)) {
         alert("Invalid name");
+        return false;
     }
     var mailRegex = /[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+/;
     if (mail.match(mailRegex)) {
@@ -13,8 +18,6 @@
     else
         {
             alert("Invalid mail id");
-            //return false;
-
+            return false;
         }
-    }
-}
+};
